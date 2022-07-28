f89ee7adc8db60331a9f9e26f473e8c0ab04c98d
Dmitry <mandrin2013@mail.ru>
2022-07-28 12:12:23 +0300

    slides current

--- js/script.js
+++ js/script.js
@@ -322,12 +322,20 @@ window.addEventListener('DOMContentLoaded', (e) => {
    }
    const slides = document.querySelectorAll('.offer__slide'),
       prev = document.querySelector('.offer__slider-prev'),
-      next = document.querySelector('.offer__slider-next');
+      next = document.querySelector('.offer__slider-next'),
+      total = document.querySelector('#total'),
+      current = document.querySelector('#current');
    console.log(slides);
 
    let slideIndex = 1;
 
    showSlides(slideIndex);
+
+   if (slides.length < 10) {
+      total.textContent = `0${slides.length}`;
+   } else {
+      total.textContent = slides.length;
+   }
 
    function showSlides(n) {
 
@@ -340,6 +348,12 @@ window.addEventListener('DOMContentLoaded', (e) => {
       slides.forEach(item => item.style.display = 'none');
 
       slides[slideIndex - 1].style.display = 'block';
+
+      if (slides.length < 10) {
+         current.textContent = `0${slideIndex}`;
+      } else {
+         current.textContent = slideIndex;
+      }
    }
    function plusSlides(n) {
       showSlides(slideIndex += n);
