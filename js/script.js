8ea542f5c09785499038e5a3e98844209b7d5576
Dmitry <mandrin2013@mail.ru>
2022-08-03 10:25:39 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -61,7 +61,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-
+function setOptions(height, width, ...additional) {
+   console.log(height, width, ...additional)
+}
+setOptions(400, 500, 'red', 'top');
 
 
    //calculator
